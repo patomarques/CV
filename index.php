9678e16372c53fb25b5c099e4a3a082257faf5cf
Dayvson Marques <dayvson.marques@gmail.com>
2017-03-01 21:19:35 -0300

    initial commit

---
+++ index.php
@@ -0,0 +1,354 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DAYVSON MARQUES | DESENVOLVEDOR WEB</title>
+	<link rel="stylesheet" type="text/css" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
+
+	<link href="https://fonts.googleapis.com/css?family=Fauna+One" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Marcellus" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Sahitya" rel="stylesheet">
+
+	<link rel="stylesheet" href="css/cv17.css">
+
+
+</head>
+<body>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-md-2 text-center">
+				<span class="caracter-title">{</span>
+			</div>
+			<div class="col-md-8 text-center">
+				<h1 class="title-name">DAYVSON MARQUES</h1>
+				<h2 class="profession">DESENVOLVEDOR WEB * FULL STACK</h2>				
+				<ul class="list-inline info-headers">
+					<li>
+						<p>27 ANOS</p>	
+					</li>
+					<li>
+						<p><i class="fa fa-map-marker" aria-hidden="true"></i> RECIFE / PE</p>
+					</li>
+					<li>						
+						 <i class="fa fa-paper-plane" aria-hidden="true"></i>
+						 <a href="mailto:dayvson.marques@gmail.com">dayvson.marques@gmail.com</a>
+					</li>
+					<li>
+						<p><i class="fa fa-whatsapp" aria-hidden="true"></i> 81 9 9962.3374</p>
+					</li>
+				</ul>	
+				<ul class="list-inline list-unstyled info-contact">
+						<li>
+							<a href="http://lnked.in/dayvsonmarques" title="Linkedin - Perfil">
+								<i class="fa fa-linkedin-square" aria-hidden="true"></i>
+								http://lnked.in/dayvsonmarques	
+							</a>
+						</li>
+						<!-- <li> | </li> -->
+						<li>
+							<a href="kaweek.net/dayvsonmarques" target="_blank" title="Portfólio Kaweek">							
+								<i class="fa fa-code" aria-hidden="true"></i>
+								Portfólio: kawek.net/dayvsonmarques							
+							</a>
+						</li>						
+					</ul>	
+			</div>
+			<div class="col-md-2 text-center">
+				<span class="caracter-title">}</span>
+			</div>
+			<div class="col-md-4 img-perfil hidden">
+				<i class="fa fa-user-circle-o" aria-hidden="true"></i>
+			</div>
+		</div>		
+	</div>
+
+	<section id="sobre" class="container content-sahitya">
+		<div class="row">
+			<div class="col-md-12">
+				<h2 class="title"><i class="fa fa-user-circle-o" aria-hidden="true"></i> Sobre Mim</h2>		
+			</div>			
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+					<p class="conteudo">Desenvolvedor web desde 2010, com experiência em backend e frontend.<br>
+					Entusiasta em tecnologia opensource, wordpress e soluções voltadas para comércio online.<br>
+					Desenvolvo sites responsivos, aplicações web & lojas online, corrijo bugs e faço café.<br>
+					Concepção | Criação | Implementação | Códificação | Integração | Testes | Prototipação</p>			
+			</div>
+		</div>	
+	</section>
+
+	
+	<section id="educacional" class="container content-sahitya">
+		<div class="row">
+			<div class="col-md-12">
+				<h2 class="title"><i class="fa fa-graduation-cap" aria-hidden="true"></i> Educacional</h2>
+			</div>
+		</div>
+	
+		<ul class="list-unstyled row">
+			<li class="col-md-6">					
+				<h3>Bacharelado em Sistemas de Informação</h3>
+				<h4><i class="fa fa-university" aria-hidden="true"></i> Faculdade Joaquim Nabuco - Recife</h4>
+				<p><i class="fa fa-clock-o" aria-hidden="true"></i> Cursado no período entre Ago/2008 | Jun/2012</p>	
+			</li>
+			<li class="col-md-6">					
+				<h3>Curso de desenvolvimento (java web)</h3>				
+				<h4><i class="fa fa-university" aria-hidden="true"></i> Softex Recife</h4>
+				<p><i class="fa fa-clock-o" aria-hidden="true"></i> Concluído em Ago/2013 (150 horas)</p>	
+			</li>
+		</ul>
+		
+	</section>
+
+	<section id="habilidades" class="container content-sahitya">
+		<div class="row">
+			<div class="col-md-8">
+				<h2 class="title"><i class="fa fa-terminal" aria-hidden="true"></i> Habilidades</h2>
+			</div>			
+			<div class="col-md-4">
+				<h2 class="title"><i class="fa fa-wrench" aria-hidden="true"></i> Ferramentas</h2>
+			</div>
+		</div>
+		<div class="row">
+			<ul class="list-unstyled col-md-4 list-skills">
+				<li>
+					<div class="pull-left">
+						html 5
+					</div>
+					<div class="pull-right">
+						<?php for($i=0; $i < 7; $i++){ ?>
+						<i class="fa fa-star" aria-hidden="true"></i>							
+						<?php } ?>
+						<?php for($i=0; $i < 3; $i++){ ?>
+						<i class="fa fa-star-o" aria-hidden="true"></i>
+						<?php } ?>
+					</div>
+				</li>
+				<li>
+					<div class="pull-left">
+						css 3 / sass
+					</div>
+					<div class="pull-right">
+						<?php for($i=0; $i < 6; $i++){ ?>
+						<i class="fa fa-star" aria-hidden="true"></i>							
+						<?php } ?>
+						<i class="fa fa-star-half-o" aria-hidden="true"></i>
+						<?php for($i=0; $i < 3; $i++){ ?>
+						<i class="fa fa-star-o" aria-hidden="true"></i>
+						<?php } ?>
+					</div>
+				</li>
+				<li>
+					<div class="pull-left">
+						javascript
+					</div>
+					<div class="pull-right">
+						<?php for($i=0; $i < 7; $i++){ ?>
+						<i class="fa fa-star" aria-hidden="true"></i>							
+						<?php } ?>
+						<i class="fa fa-star-half-o" aria-hidden="true"></i>
+						<?php for($i=0; $i < 2; $i++){ ?>
+						<i class="fa fa-star-o" aria-hidden="true"></i>
+						<?php } ?>
+					</div>
+				</li>
+				<li>
+					<div class="pull-left">
+						php oo
+					</div>
+					<div class="pull-right">
+						<?php for($i=0; $i < 8; $i++){ ?>
+						<i class="fa fa-star" aria-hidden="true"></i>							
+						<?php } ?>						
+						<?php for($i=0; $i < 2; $i++){ ?>
+						<i class="fa fa-star-o" aria-hidden="true"></i>
+						<?php } ?>
+					</div>
+				</li>
+				<li>
+					<div class="pull-left">
+						sql
+					</div>
+					<div class="pull-right">
+						<?php for($i=0; $i < 9; $i++){ ?>
+						<i class="fa fa-star" aria-hidden="true"></i>							
+						<?php } ?>								
+						<?php for($i=0; $i < 1; $i++){ ?>
+						<i class="fa fa-star-o" aria-hidden="true"></i>
+						<?php } ?>
+					</div>
+				</li>
+				<li>
+					<div class="pull-left">
+						design responsivo
+					</div>
+					<div class="pull-right">
+						<?php for($i=0; $i < 8; $i++){ ?>
+						<i class="fa fa-star" aria-hidden="true"></i>							
+						<?php } ?>								
+						<?php for($i=0; $i < 2; $i++){ ?>
+						<i class="fa fa-star-o" aria-hidden="true"></i>
+						<?php } ?>
+					</div>
+				</li>
+				
+			</ul>
+			<ul class="list-unstyled col-md-4 list-skills">				
+				<li>
+					<div class="pull-left">
+						Wordpress
+					</div>
+					<div class="pull-right">
+						<?php for($i=0; $i < 8; $i++){ ?>
+						<i class="fa fa-star" aria-hidden="true"></i>							
+						<?php } ?>																			
+						<?php for($i=0; $i < 2; $i++){ ?>
+						<i class="fa fa-star-o" aria-hidden="true"></i>
+						<?php } ?>
+					</div>
+				</li>
+				<li>
+					<div class="pull-left">
+						Woocommerce
+					</div>
+					<div class="pull-right">
+						<?php for($i=0; $i < 6; $i++){ ?>
+						<i class="fa fa-star" aria-hidden="true"></i>							
+						<?php } ?>								
+						<i class="fa fa-star-half-o" aria-hidden="true"></i>							
+						<?php for($i=0; $i < 3; $i++){ ?>
+						<i class="fa fa-star-o" aria-hidden="true"></i>
+						<?php } ?>
+					</div>
+				</li>
+				<li>
+					<div class="pull-left">
+						Opencart 2+
+					</div>
+					<div class="pull-right">
+						<?php for($i=0; $i < 8; $i++){ ?>
+						<i class="fa fa-star" aria-hidden="true"></i>							
+						<?php } ?>			
+
+						<?php for($i=0; $i < 2; $i++){ ?>
+						<i class="fa fa-star-o" aria-hidden="true"></i>
+						<?php } ?>
+					</div>
+				</li>
+				<li>
+					<div class="pull-left">
+						Laravel 5.3+
+					</div>
+					<div class="pull-right">
+						<?php for($i=0; $i < 6; $i++){ ?>
+						<i class="fa fa-star" aria-hidden="true"></i>							
+						<?php } ?>								
+						<?php for($i=0; $i < 4; $i++){ ?>
+						<i class="fa fa-star-o" aria-hidden="true"></i>
+						<?php } ?>
+					</div>
+				</li>
+				<li>
+					<div class="pull-left">
+						bootstrap 3+
+					</div>
+					<div class="pull-right">
+						<?php for($i=0; $i < 8; $i++){ ?>
+						<i class="fa fa-star" aria-hidden="true"></i>							
+						<?php } ?>									
+						<?php for($i=0; $i < 2; $i++){ ?>
+						<i class="fa fa-star-o" aria-hidden="true"></i>
+						<?php } ?>
+					</div>
+				</li>
+				<li>
+					<div class="pull-left">
+						JQuery
+					</div>
+					<div class="pull-right">
+						<?php for($i=0; $i < 9; $i++){ ?>
+						<i class="fa fa-star" aria-hidden="true"></i>							
+						<?php } ?>								
+						<?php for($i=0; $i < 1; $i++){ ?>
+						<i class="fa fa-star-o" aria-hidden="true"></i>
+						<?php } ?>					
+					</div>
+				</li>
+				<li>
+					<div class="pull-left">
+						Vue.js
+					</div>
+					<div class="pull-right">
+						<?php for($i=0; $i < 5; $i++){ ?>
+						<i class="fa fa-star" aria-hidden="true"></i>							
+						<?php } ?>			
+						<i class="fa fa-star-half-o" aria-hidden="true"></i>
+						<?php for($i=0; $i < 4; $i++){ ?>
+						<i class="fa fa-star-o" aria-hidden="true"></i>
+						<?php } ?>				
+					</div>
+				</li>
+			</ul>		
+			
+			
+			<div class="col-md-4">
+				<ul class="list-unstyled">
+					<li>PhpStorm IDE</li>	
+					<li>MySQL Workbench</li>
+					<li>git | bower | composer</li>					
+					<li>Integração gateway pagamento <br> { PagSeguro & CIELO }</li>
+					<li>Métodologia Ágeis : { SCRUM & XP }</li>
+					<li>Banco de dados: { MySQL y PostgreeSQL }</li>
+				</ul>
+
+			</div>
+
+		</div>	
+	</section>
+
+	<section id="experiencia" class="container text-justify content-sahitya">
+		<div class="row">
+			<div class="col-md-12">
+				<h2 class=title><i class="fa fa-laptop" aria-hidden="true"></i> Experiência</h2>	
+			</div>
+		</div>		
+
+		<ul class="list-unstyled row">
+			<li class="col-md-6 trampo">
+				<h4 class="title-experience">Bold Comunicação</h4>
+				<p><b>Cargo:</b> Desenvolvedor Web </p>
+				<p><b>Período:</b> Dez/2015 - Fev/2017</p>
+				<p><b>Tecnologias:</b> Wordpress, Opencart, Laravel 5+, PHP OO, Bootstrap, html5, css3/sass, javascrit/JQuery/Vue.js;</p>
+				<p><b>Atividades:</b> desenvolvimento de sites e lojas virtuais; mapeamento, criação e gerenciamento do banco de dados; implementação do backend (mvc); construção de frontend : códificar de PSD para html5/css3; testar responsivo / cross browser. integração com api's de pagamento (pagseguro & cielo) e redes sociais (fb & ig); Analise de performance & SEO.</p>
+			</li>
+			<li class="col-md-6 trampo">
+				<h4 class="title-experience">Veleja</h4>
+				<p><b>Cargo: </b>Desenvolvedor Backend (Freelancer)</p>
+				<p><b>Período: </b>Fev/2015 - Jun/2015</p>
+				<p><b>Tecnologias: </b>OpenCart, PHP, html5, css3, javascript & MySQL;</p>
+				<p><b>Atividades: </b>Desenvolvimento de funcionalidades, manutenção & correção de bugs ; implementar e customizar backend de lojas online com OpenCart (framework de ecommerce PHP).</p>
+			</li>
+		</ul>
+		<ul class="row list-unstyled">
+			<li class="col-md-6 trampo">
+				<h4 class="title-experience">Surface Lab</h4>
+				<p><b>Cargo: </b>Desenvolvedor web (freelancer)</p>
+				<p><b>Período: </b>Jun/2013 - Jan/2015 | Mai/2015 - Nov/2015</p>
+				<p><b>Tecnologias: </b>html5, css3, php, js/jquery, wordpress, opencart, bootstrap, design responsivo e mysql</p>
+				<p><b>Atividades: </b>desenvolvimento de sites e lojas virtuais; testes com layout responsivo e multi browser; analise & otimização SEO;</p>
+			</li>			
+			<li class="col-md-6 trampo">
+				<h4 class="title-experience">Corptech</h4>
+					<p><b>Cargo: </b>Desenvolvedor Java Web </p>
+					<p><b>Período: </b>Abr/2011 - Abr/2013</p>
+					<p><b>Tecnologias: </b>Java (J2EE), JSP/Servlets, java serverfaces & hibernate; mysql e postgreeSQL, google api (maps & charts), (x)html/css/javascript/jquery.</p>
+					<p><b>Atividades: </b>Ánalise, desenvolvimento e manutenção de funcionalidades do software web (websuite ERP) da empresa.</p>
+			</li>
+		</ul>
+	</section>
+
+
+</body>
+</html>
